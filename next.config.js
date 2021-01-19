10412d2059de7359ef6633e5ed5971e326b6d11c
Oleksandr Bilyi <obilyi@flexservice.com>
2021-01-19 16:56:58 +0200

    added loader for images

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@ module.exports = {
   target: 'serverless',
   webpack: function (config) {
     config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    })
+        test: /\.md$/,
+        use: 'raw-loader',
+      },
+      images: {
+        domains: ['assets.vercel.com'],
+      },
+    )
     return config
   },
 }
